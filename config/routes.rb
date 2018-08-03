61c616856280adee543370bd0377b971dbc4406f
Kazuki Shibata <kazuki.shibata@medicalfitness.co.jp>
2018-08-03 16:27:58 +0900

    sorcery導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
+  resources :users
+
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
