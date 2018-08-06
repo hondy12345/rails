6fd3b61b8db67ec974cfbe08770eda49d0009342
Kazuki Shibata <kazuki.shibata@medicalfitness.co.jp>
2018-08-07 00:56:12 +0900

    create内のロジック変更

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,8 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    if @user == login(params[:email], params[:password])
+    # ログインに成功するかどうか
+    if @user = login(params[:email], params[:password])
       redirect_back_or_to(:users, notice: 'Login successful')
     else
       flash.now[:alert] = 'Login failed'
